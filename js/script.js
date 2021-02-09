6a810e1abfa76139ccedcfaeabadc6d5246f727d
Mohamed-Nasser <mohamednasser2000@outlook.com>
2021-02-09 22:06:08 +0200

    final commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+(function ($) {
+    let typed = new Typed('span.txt-rotate', {
+        strings: ["We Are Enactus MUST", "We All Win"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        fadeOut: false,
+        smartBackSpeed: true,
+        loop: true
+
+    })
+})(jQuery);
